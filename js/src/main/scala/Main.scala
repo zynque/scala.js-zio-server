f8d32c32818241876540208a30365fa4c546cfbf
zynque <zynque@github.com>
2021-03-29 16:52:07 -0700

    scalajs and laminar: logging some events (still very experimental as I learn Laminar)

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -13,6 +13,15 @@ import io.circe._
 import io.circe.syntax._
 import io.circe.parser._
 
+enum ItemCommand {
+  case Refresh
+  case Create
+  case UpdateTitle(id: String, title: String)
+  case UpdateDescription(id: String, title: String)
+  case UpdateStatus(id: String, completed: Boolean)
+  case Delete(id: String)
+}
+
 object Main extends zio.App {
 
   def run(args: List[String]) =
@@ -21,30 +30,82 @@ object Main extends zio.App {
   val appLogic =
     for {
       _ <- putStrLn("Starting")
-      todoRequest <- IO.fromFuture(ec => Ajax.get("/todo"))
-      todosString = todoRequest.responseText
-      _ <- putStrLn(s"Unparsed Response: $todosString")
-      todos <- IO.fromEither(decode[List[IdentifiedTodoItem]](todosString))
-      _ <- putStrLn(s"Parsed Response: $todos")
-      renderedTodos = renderTodoItems(todos)
-      _ <- IO.effect(render(document.body, renderedTodos))
+      _ <- IO.effect(render(document.body, renderTodoItems))
     } yield ()
 
-  def renderTodoItems(items: List[IdentifiedTodoItem]) =
-    div(items.map(renderTodoItem))
+  def renderTodoItems =
+    div(
+      button(
+        "Refresh",
+        onClick.map(_ => ItemCommand.Refresh) --> eventBus.writer
+      ),
+      button(
+        "+",
+        borderRadius("40%"),
+        onClick.map(_ => ItemCommand.Create) --> eventBus.writer
+      ),
+      eventBus.events --> anyLogger,
+      get --> anyLogger,
+      refreshes --> anyLogger,
+      children <-- renderedRefreshes
+    )
+
+  val eventBus = new EventBus[ItemCommand]
+
+  val anyLogger = Observer[Any](c => dom.console.log(c))
+
+  val newTodoString = TodoItem("", "", false).asJson.noSpaces
+  val newTodoAjaxData = Ajax.InputData.str2ajax(newTodoString)
+  val postNewTodo = IO.fromFuture(_ => Ajax.post(url = "/todo", data = newTodoAjaxData))
+
+  val get = AjaxEventStream.get("/todo").map(a => a.responseText)
+
+  val refreshEvents = eventBus
+    .events
+    .filter { case ItemCommand.Refresh => true; case _ => false }
+    .startWith(ItemCommand.Refresh)
+
+  val refreshes = for {
+    _ <- refreshEvents
+    todosString <- AjaxEventStream.get("/todo").map(a => a.responseText)
+  } yield decode[List[IdentifiedTodoItem]](todosString)
+
+  val refreshesWithInit = refreshes.startWith(Right(List()))
+
+  val renderedRefreshes = refreshesWithInit.map {
+    case Left(err) => renderCirceError(err)
+    case Right(items) => items.map(renderTodoItem)
+  }
+
+  def renderCirceError(error: io.circe.Error) =
+    List(div(span(error.getMessage)))
 
   def renderTodoItem(item: IdentifiedTodoItem) =
     div(
       span(item.id),
       input(
-        value := item.title
+        value := item.title,
+        onChange.mapToValue
+          .map(title => ItemCommand.UpdateTitle(item.id, title)) -->
+            eventBus.writer
       ),
       input(
-        value := item.description
+        value := item.description,
+        onChange.mapToValue
+          .map(description => ItemCommand.UpdateDescription(item.id, description)) -->
+            eventBus.writer
       ),
       input(
         typ := "checkbox",
-        checked := item.completed
+        checked := item.completed,
+        onClick.mapToChecked
+          .map(checked => ItemCommand.UpdateStatus(item.id, checked)) -->
+            eventBus.writer
+      ),
+      button(
+        "X",
+        borderRadius("40%"),
+        onClick.map(_ => ItemCommand.Delete(item.id)) --> eventBus.writer
       )
     )
 }
